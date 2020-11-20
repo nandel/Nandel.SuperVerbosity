f16597e3fffc38e1aa0fc64ee4f1dc0bc6e9532c
Fernando Souza <fefso1909@gmail.com>
2020-11-19 22:31:32 -0300

    feat: ICollection.AddIfNotExists

---
+++ src/Nandel.SuperVerbosity/System.Collections.Generic/CollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Collections.Generic
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Add an item to the list if no other equal item exists
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="item"></param>
+        public static void AddIfNotExists<T>(this ICollection<T> collection, T item)
+        {
+            if (!collection.Any(x => x.Equals(item))) 
+            {
+                collection.Add(item);
+            }
+        }
+    }
+}
---
+++ test/Nandel.SuperVerbosity.Tests/System.Collections.Generic/CollectionExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly;
+using Xunit;
+
+namespace Nandel.SuperVerbosity.Tests.System.Collections.Generic
+{
+    public class CollectionExtensionsTests
+    {
+        [Theory]
+        [InlineData("asd", "asd")]
+        [InlineData(1, 1)]
+        [InlineData(1L, 1L)]
+        public void AddIfNotExists_OnMultipleAddOfSameItem_ShouldNotDuplicateTheItemInTheList(object item1, object item2)
+        {
+            var list = new List<object>();
+
+            list.AddIfNotExists(item1);
+            list.AddIfNotExists(item2);
+
+            item1.Equals(item2).ShouldBeTrue();
+            list.Count.ShouldBe(1);
+        }
+    }
+}
